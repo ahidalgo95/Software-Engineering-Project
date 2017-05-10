f3c43a8dd6ea557a463df12da890a8ed1d490e70
Kate Van Donge <katemvd@gmail.com>
2017-05-09 18:43:23 -0700

    widgets suck

---
+++ app/src/main/java/com/example/katevandonge/dejaphotoproject/NewAppWidget.java
@@ -0,0 +1,68 @@
+package com.example.katevandonge.dejaphotoproject;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RemoteViews;
+import android.app.PendingIntent;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.widget.RemoteViews;
+import static android.R.attr.button;
+
+import android.Manifest;
+
+import android.widget.Button;
+import android.widget.Toast;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class NewAppWidget extends AppWidgetProvider {
+    public static String WIDGET_BUTTON = "MY_PACKAGE_NAME.WIDGET_BUTTON";
+
+
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+        String but = "hello world!";
+
+        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
+        views.setTextViewText(R.id.appwidget_text, but);
+
+        //Intent intent = new Intent(WIDGET_BUTTON);
+        Intent intent = new Intent(context, NewAppWidget.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
+                0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+        views.setOnClickPendingIntent(R.id.button6, pendingIntent);
+
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+
+}
+
