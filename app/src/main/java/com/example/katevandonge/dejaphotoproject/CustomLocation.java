767e15e37211aa2d1f8f4d4dac60e6b2a7c085f4
adriancordovayquiroz <adriancyq@gmail.com>
2017-05-31 15:57:30 -0700

    custom location

---
+++ app/src/main/java/com/example/katevandonge/dejaphotoproject/CustomLocation.java
@@ -0,0 +1,85 @@
+package com.example.katevandonge.dejaphotoproject;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.support.v4.content.FileProvider;
+
+
+import java.io.File;
+
+public class CustomLocation extends AppCompatActivity {
+
+    private static final int SELECTED_PIC = 1;
+
+    int counter = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_location);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        Button switchScreen = (Button) findViewById(R.id.button4);
+        switchScreen.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                finish();
+            }
+        });
+    }
+
+    public void btnClick(View view){
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, SELECTED_PIC);
+    }
+
+    public void btnClick2(View view){
+        File fileDir = new File(Environment.getExternalStorageDirectory()+File.separator+".privPhotos");
+        Uri fileUri = FileProvider.getUriForFile(getApplicationContext(), getApplicationContext().getPackageName() + ".provider", fileDir);
+        Intent intent = new Intent(Intent.ACTION_PICK, fileUri);
+        startActivityForResult(intent, SELECTED_PIC);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        switch(requestCode){
+            case SELECTED_PIC:
+                if(resultCode == RESULT_OK){
+                    Uri uri = data.getData();
+                    String[] projection = {MediaStore.Images.Thumbnails.DATA};
+                    Cursor cursor = getContentResolver().query(uri,projection,null,null,null);
+                    cursor.moveToFirst();
+                    int columnIndex = cursor.getColumnIndex(projection[0]);
+                    String filepath=cursor.getString(columnIndex);
+                    cursor.close();
+
+                    Bitmap bitmap = BitmapFactory.decodeFile(filepath);
+                    Drawable drawable = new BitmapDrawable(bitmap);
+
+                    CopiedGallery.addPhoto(uri);
+
+                }
+                break;
+            default:
+                break;
+        }
+    }
+}
