22231ce332d31227372c187e5d62b6a41fd700dd
adriancordovayquiroz <adriancyq@gmail.com>
2017-06-08 18:45:19 -0700

    CopiedGalleryTest

---
+++ app/src/androidTest/java/com/example/katevandonge/dejaphotoproject/CopiedGalleryTest.java
@@ -0,0 +1,30 @@
+package com.example.katevandonge.dejaphotoproject;
+
+import android.content.Context;
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by adriancordovayquiroz on 6/8/17.
+ */
+
+public class CopiedGalleryTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivity= new ActivityTestRule<MainActivity>(MainActivity.class);
+
+    @Test
+    public void testConstructor(){
+        Context context= mainActivity.getActivity().getApplicationContext();
+        CopiedGallery gallery= new CopiedGallery();
+        assertEquals(gallery.copiedQueue.poll(), null);
+        assertEquals(gallery.copiedAL.size(), 0);
+    }
+
+
+
+}
